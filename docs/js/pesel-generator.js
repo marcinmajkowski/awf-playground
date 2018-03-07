fa90ebd757047c5e030143ca5e7e804bf5788fb5
Marcin Majkowski <pl.marcin.m@gmail.com>
2018-03-07 21:15:47 +0100

    Implement workaround so m-radio-segmented UI gets updated correctly

--- docs/js/pesel-generator.js
+++ docs/js/pesel-generator.js
@@ -22,13 +22,17 @@ define(['jquery', 'app/pesel-service'], function ($, peselService) {
                         that.updateForm($wrapper);
                     });
 
+                $wrapper.isHandleFormChange = true;
+
                 $()
                     .add($wrapper.$sexRadios)
                     .add($wrapper.$ageInputMethodRadios)
                     .add($wrapper.$ageInput)
                     .add($wrapper.$birthdateInput)
                     .change(function () {
-                        that.updatePesel($wrapper);
+                        if ($wrapper.isHandleFormChange) {
+                            that.updatePesel($wrapper);
+                        }
                     });
 
                 that.updatePesel($wrapper);
@@ -45,13 +49,25 @@ define(['jquery', 'app/pesel-service'], function ($, peselService) {
         },
 
         updateForm: function ($wrapper) {
+            // This is a workaround due to need to trigger change for awf UI to update.
+            $wrapper.isHandleFormChange = false;
+
             var parsedPesel = peselService.parse($wrapper.$peselInput.val());
-            // FIXME awf expects radio to be checked only in UI
+            this.updateSexRadio($wrapper, parsedPesel.sex);
+            $wrapper.$ageInput.val(parsedPesel.age);
+            $wrapper.$birthdateInput.val(parsedPesel.birthdate);
+
+            $wrapper.isHandleFormChange = true;
+        },
+
+        updateSexRadio: function ($wrapper, sex) {
             $wrapper.$sexRadios.each(function () {
                 var $radio = $(this);
-                $radio.prop('checked', $radio.val() === parsedPesel.sex);
+                $radio.prop('checked', $radio.val() === sex);
             });
-            // TODO set remaining controls
+            // FIXME this cause updatePesel call
+            // This is required for m-radio-segmented UI update.
+            $wrapper.$sexRadios.first().change();
         }
     };
 
